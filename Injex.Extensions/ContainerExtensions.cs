0bf364152e19ca3e3ff1d5d1c26a97e30ec6ca50
FFad3 <gnebapatryk@gmail.com>
2025-09-28 20:55:55 +0200

    feat: extenstions

---
+++ Injex.Extensions/ContainerExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+using Injex.Abstractions;
+
+namespace Injex.Extensions;
+
+public static class ContainerExtensions
+{
+    public static T? GetService<T>(this IContainer container)
+        => (T?)container.GetService(typeof(T));
+}
